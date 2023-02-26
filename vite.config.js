7c6caaab16acaa0d393cdb73d495b8a6617f2495
Pobitrodeb <pobitrodn@gmail.com>
2023-02-26 20:00:57 +0600

    successfuly setup vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
+// export default defineConfig({
+//     plugins: [
+//         vue(),
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        vue(),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 });
+
