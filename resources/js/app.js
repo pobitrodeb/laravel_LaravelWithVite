a325ce564a999eecd543ca4388e8585fd39263a7
Pobitrodeb <pobitrodn@gmail.com>
2023-02-26 20:45:53 +0600

    bootstrap setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import './bootstrap';
 import App from './App.vue'
+import '../sass/app.scss'
 
 const app = createApp(App)
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// export default defineConfig({
-//     plugins: [
-//         vue(),
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
 export default defineConfig({
     plugins: [
-        vue(),
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+
+        }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
